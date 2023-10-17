5f1575332ca71350070ea7981a41a6ca38a836c5
ryotadora1225 <131091974+ryotadora1225@users.noreply.github.com>
2023-10-17 12:17:26 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,5 +1,5 @@
 var who65='world';
 
-function sayhello65(){alert('HEllo, '+ who65+ '!');}
+function sayhello65(){alert('HEllo,'+ who65+ '!');}
 function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
