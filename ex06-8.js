0b18413660c4f509544967a8320366718379250d
ryotadora1225 <131091974+ryotadora1225@users.noreply.github.com>
2023-10-31 10:39:33 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,3 +1,4 @@
+var who68='world';
 function sayhello68(){alert('Hello,'+document.getElementByld('who68').innerHTML+'!');}
 function taro68(){document.getElementByld('who68').innerHTML='太郎';}
 function hanako68(){document.getElementByld('who68').innerHTML='花子';}
