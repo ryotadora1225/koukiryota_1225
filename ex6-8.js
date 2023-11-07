edcc7cf8f22f835b64743a072a7f246fad40f3c5
ryotadora1225 <131091974+ryotadora1225@users.noreply.github.com>
2023-11-07 10:16:35 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -2,4 +2,4 @@ function sayhello68(){alert('Hello,'+document.getElementById('who68').innerHTML+
 function taro68(){document.getElementById('who68').innerHTML='太郎';}
 function hanako68(){document.getElementById('who68').innerHTML='花子';}
 
-function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/soccer_ball.png" alt="サッカーボール">';}
