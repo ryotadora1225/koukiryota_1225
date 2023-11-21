151c15ebe114b83aa96331b9c806971ed1f68ec5
ryotadora1225 <131091974+ryotadora1225@users.noreply.github.com>
2023-11-21 10:22:05 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,2 +1,2 @@
 function imgchange3(){document.getElementById('logo').src='./image/img_josai02.png';}
-function imgchange4(){document.getElementById('logo').src='./image/img_josai02.png';}
+function imgchange4(){document.getElementById('logo').src='./image/img_josai01.png';}
