fbb654609e1c21e802760d07eb7aa08fd6322fcf
ryotadora1225 <131091974+ryotadora1225@users.noreply.github.com>
2023-12-12 10:30:00 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange5(){document.getElementById('logo2').src='./image/img_02.png';}
+function imgchange5(){document.getElementById('logo2').src='./image/img_02.png';setTimeout("imgchange6()",1000);}
 function imgchange6(){document.getElementById('logo2').src='./image/img_01.png';}
